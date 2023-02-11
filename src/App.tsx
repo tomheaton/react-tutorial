926710839349d61a408deab412a00fb04f51afa5
Tom Heaton <thomas.m.heaton@gmail.com>
2023-02-11 13:36:35 +0000

    search etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { type FormEvent, useState } from "react";
+import { type FormEvent, useState, useMemo } from "react";
+
+type Item = {
+  value: string;
+  checked: boolean;
+};
 
 const App: React.FC = () => {
-  const [items, setItems] = useState<string[]>([]);
+  const [items, setItems] = useState<Item[]>([]);
   const [input, setInput] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
 
   const handleAddItem = (e: FormEvent) => {
     e.preventDefault();
     console.log("adding item...");
-    setItems([...items, input]);
+    setItems([...items, { value: input, checked: false }]);
     setInput("");
   };
 
@@ -15,6 +21,23 @@ const App: React.FC = () => {
     console.log("removing item...");
     setItems(items.filter((_, i) => i !== index));
   };
+
+  const handleDoneItem = (index: number) => {
+    console.log("done item...");
+    setItems(
+      items.map((item, i) =>
+        i === index ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const filteredItems = useMemo(
+    () =>
+      items.filter((item) =>
+        item.value.toLowerCase().includes(search.toLowerCase())
+      ),
+    [search, items]
+  );
 
   return (
     <div className="w-full h-screen flex pt-20 justify-evenly">
@@ -42,20 +65,43 @@ const App: React.FC = () => {
       <div className="w-1/4 flex flex-col">
         <p className="font-bold">items</p>
         <br />
-        {!items.length && <p>no items</p>}
+        <input
+          type="text"
+          className="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          placeholder="apples, oranges, etc."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <br />
+        {!filteredItems.length && <p>no items</p>}
+        {/* <ul className="list-disc space-y-2 overflow-y-scroll"> */}
         <ul className="list-disc space-y-2">
-          {items.map((item, index) => (
+          {filteredItems.map((item, index) => (
             <li
               key={index}
               className="space-x-2 flex items-center justify-between border-2 border-gray-300 rounded-lg p-2"
             >
-              <p className="break-all">{item}</p>
-              <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
-                onClick={() => handleRemoveItem(index)}
-              >
-                remove
-              </button>
+              <p className={`break-all ${item.checked && "line-through"}`}>
+                {item.value}
+              </p>
+              <div className="space-x-2">
+                <button
+                  className={`${
+                    item.checked
+                      ? "bg-green-500 hover:bg-green-700"
+                      : "bg-orange-500 hover:bg-orange-700"
+                  } text-white font-bold py-2 px-4 rounded-lg`}
+                  onClick={() => handleDoneItem(index)}
+                >
+                  {item.checked ? "done" : "not done"}
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
+                  onClick={() => handleRemoveItem(index)}
+                >
+                  remove
+                </button>
+              </div>
             </li>
           ))}
         </ul>
